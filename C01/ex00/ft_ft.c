9ce365701d1eb426427543154528e5abf65970ae
vacarval <vacarval@LinuxUbuntu.myguest.virtualbox.org>
2023-07-27 19:36:43 +0100

    Ex01/Ex02

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,23 @@
+void	ft_ft(int *nbr)
+
+{
+	*nbr = 42;
+}
+
+#include <stdio.h>
+
+/*
+int	main(void)
+{
+	int a;
+	int *ptr;
+
+	ptr = &a;
+	
+	printf("pointer value before %d\n", a);
+	
+	ft_ft(ptr);
+
+	printf("pointer value now %d\n", a);
+}
+*/
---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,29 @@
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*
+ #include <stdio.h>
+
+int	main(void)
+{
+	int	n;
+	int	*nbr1 = &n;
+	int	**nbr2 = &nbr1;
+	int	***nbr3 = &nbr2;
+	int	****nbr4 = &nbr3;
+	int	*****nbr5 = &nbr4;
+	int	******nbr6 = &nbr5;
+	int	*******nbr7 = &nbr6;
+	int	********nbr8 = &nbr7;
+	int	*********nbr = &nbr8;
+	
+	n = 1;
+
+	printf("pointer value before %d\n", n);
+	ft_ultimate_ft(nbr);
+	printf("pointer value after pointers %d\n", n);
+	return 0;
+}
+*/
