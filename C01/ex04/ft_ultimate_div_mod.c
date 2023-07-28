650385f275a6939019e9457c03e2ea8b95313a7a
vacarval <vacarval@LinuxUbuntu.myguest.virtualbox.org>
2023-07-28 12:12:48 +0100

    exercise 3 and 4

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,26 @@
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a;
+	mod = *b;
+	*a = div / mod;
+	*b = div % mod;
+}
+#include <stdio.h>
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 10;
+	b = 5;
+
+	printf("a : %d, b : %d\n", a, b);
+
+	ft_ultimate_div_mod(&a, &b);
+
+	printf("a : %d, b : %d\n", a, b);
+}
