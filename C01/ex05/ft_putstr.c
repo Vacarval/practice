3cf590607d33e3a2d4ba7f8554073ea857beb9fb
vacarval <vacarval@LinuxUbuntu.myguest.virtualbox.org>
2023-07-28 12:37:59 +0100

    ex05

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str [len])
+		len++;
+	write(1, str, len);
+}
+/*
+int	main(void)
+{
+	ft_putstr("Lucky you");
+}
+*
+*/
